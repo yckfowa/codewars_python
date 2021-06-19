51eccc8c5a690ba866ae64c23cc83d86dff73afe
yckfowa <80372122+yckfowa@users.noreply.github.com>
2021-06-19 10:07:22 +0800

    Update Isograms.py

--- 7KYU/Isograms.py
+++ 7KYU/Isograms.py
@@ -1,11 +1,5 @@
 def is_isogram(string):
     string = string.lower()
-
-    list = []
-
     for letter in string:
-        if letter in list:
-            return False
-        list.append(letter)
-
+        if string.count(letter) > 1: return False
     return True
