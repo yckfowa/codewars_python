d87e775b522841f3d4ad2cc7e002b9a0222b86c5
yckfowa <80372122+yckfowa@users.noreply.github.com>
2021-06-22 09:24:33 +0800

    Solution for task: Number of People in the Bus

--- 7KYU/Number of People in the Bus.py
+++ 7KYU/Number of People in the Bus.py
@@ -1,3 +1,4 @@
+#ver.1
 def number(bus_stops):
     first_total = 0
     sec_total = 0
@@ -6,3 +7,9 @@ def number(bus_stops):
         first_total += bus_stops[i][0]
         sec_total += bus_stops[i][1]
     return first_total - sec_total
+
+---------------------------------------
+#ver.2 quicker way 
+
+def number(bus_stops):
+    return sum([stop[0] - stop[1] for stop in bus_stops])
