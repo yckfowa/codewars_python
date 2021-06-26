c20f608c0315ac1c2ae08e5bd23cf6ec58346f7e
yckfowa <80372122+yckfowa@users.noreply.github.com>
2021-06-26 09:51:48 +0800

    Solution for task: Take a Ten Minute Walk

---
+++ 6KYU/Take a Ten Minute Walk.py
@@ -0,0 +1,6 @@
+def is_valid_walk(walk):
+    if (walk.count('n') == walk.count('s') and
+            walk.count('e') == walk.count('w') and
+            len(walk) == 10):
+        return True
+    return False
