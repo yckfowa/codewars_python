32071b74f46c798b36de5f19b21ce2aa8e202d2c
yckfowa <80372122+yckfowa@users.noreply.github.com>
2021-06-28 14:41:24 +0800

    Solution for task: Is a number prime?

---
+++ 6KYU/Is a number prime?.py
@@ -0,0 +1,9 @@
+import math
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2,int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
