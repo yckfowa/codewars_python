c484c02c6ed02ef38d5cb583e4786f193cf4120b
yckfowa <80372122+yckfowa@users.noreply.github.com>
2021-07-18 15:19:42 +0800

    Solution for task: String incrementer

---
+++ 5KYU/String incrementer.py
@@ -0,0 +1,12 @@
+
+"""
+write a function which increments a string, to create a new string.
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+
+"""
+def increment_string(strng):
+    head = strng.rstrip('0123456789')
+    tail = strng[len(head):]
+    if tail == "": return strng+"1"
+    return head + str(int(tail) + 1).zfill(len(tail))  #zfill add zero to the beginning of the string until it reaches certain length
